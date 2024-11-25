9e771de4f8bc4fd5d2a8337e14994b15e0d5ac5f
Juan <juanjokun2002@gmail.com>
2024-11-25 13:58:09 -0400

    mysql_remoto?

--- admin/bd/connection.php
+++ admin/bd/connection.php
@@ -1,6 +1,7 @@
 <?php
 
-$con = new mysqli("localhost","root","","electrocarly");
+#$con = new mysqli("localhost","root","","electrocarly");
+$con = new mysqli("mysql.railway.internal","root","EevbTxHbTgsciPnIobUJYMARRaBAXYBe","railway");
 $con->set_charset("utf8");
 
 ?>
