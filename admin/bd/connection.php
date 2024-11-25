06a7dfecafcb240f3653b4519f35a0feca8527ee
Juan <juanjokun2002@gmail.com>
2024-11-25 14:16:39 -0400

    SQL_remotoXD

--- admin/bd/connection.php
+++ admin/bd/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 #$con = new mysqli("localhost","root","","electrocarly");
-$con = new mysqli("mysql.railway.internal","root","EevbTxHbTgsciPnIobUJYMARRaBAXYBe","railway");
+$con = new mysqli("bxazrgknaogpic9zs3xf-mysql.services.clever-cloud.com","u0lwdmkdqqdc0yrr","yIwJXBNh8OFceY7Fwg90","bxazrgknaogpic9zs3xf");
 $con->set_charset("utf8");
 
 ?>
